6014758d359e65ca783a0ea80b95b0b97ff00760
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-08 18:32:06 +0900

    init storybook & rename packages pattern

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    "../packages/code-ui-*/**/*.stories.mdx",
+    "../packages/code-ui-*/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
