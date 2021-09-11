0eac0921cfef17eb370748a1a479521f5dd6c71f
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-11 21:36:26 +0900

    add coli-code-view demo

--- packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
+++ packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { BasedToken } from "@code-ui/token";
+import { ColiObject, stringfy } from "coli";
+import { get_color_scheme_for_syntax_kind } from "../color-scheme-mappings";
+import { SyntaxKind } from "@coli.codes/core-syntax-kind";
 
-export function ColiCodeView(props: {
-  coli: any; // ColiObject
+export function ColiCodeView({
+  coli,
+  fallbackContentColor = "white",
+  language = "typescript",
+}: {
+  coli: ColiObject;
+  fallbackContentColor?: string;
+  language?: "typescript";
 }) {
-  //
+  const maybe_syntax_kind = coli.__type;
+  const color =
+    get_color_scheme_for_syntax_kind(maybe_syntax_kind as SyntaxKind) ??
+    fallbackContentColor;
+
+  const code_string = stringfy(coli, { language: language });
+
   return (
     <BasedToken
       onClick={function (): void {
@@ -15,8 +30,9 @@ export function ColiCodeView(props: {
       }}
       cornerRadius={0}
       contentPadding={0}
-      contentColor={""}
-      content={""}
+      backgroundColor="black"
+      contentColor={color}
+      content={code_string}
     />
   );
 }
