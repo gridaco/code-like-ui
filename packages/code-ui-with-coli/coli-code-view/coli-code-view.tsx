cca98a35af5910d092928259f2c725ecf6aa9c8a
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-12 14:30:02 +0900

    add tree handling for coli view

--- packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
+++ packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
@@ -13,26 +13,51 @@ export function ColiCodeView({
   fallbackContentColor?: string;
   language?: "typescript";
 }) {
-  const maybe_syntax_kind = coli.__type;
-  const color =
-    get_color_scheme_for_syntax_kind(maybe_syntax_kind as SyntaxKind) ??
-    fallbackContentColor;
+  const tree = (coli) => {
+    const all_fields = Object.keys(coli);
+    const composisions = all_fields
+      .map((k) => {
+        console.log(k);
+        const field = coli[k];
+        if (field instanceof ColiObject) {
+          return <ColiCodeView key={field.__type} coli={field} />;
+        } else if (instanceArrayOf(field, ColiObject)) {
+          return field.map((c) => tree(c));
+        }
+      })
+      .filter((x) => x !== undefined);
+    return composisions;
+  };
 
-  const code_string = stringfy(coli, { language: language });
+  const composisions = tree(coli).flat();
 
-  return (
-    <BasedToken
-      onClick={function (): void {
-        throw new Error("Function not implemented.");
-      }}
-      onDoubleClick={function (): void {
-        throw new Error("Function not implemented.");
-      }}
-      cornerRadius={0}
-      contentPadding={0}
-      backgroundColor="black"
-      contentColor={color}
-      content={code_string}
-    />
-  );
+  if (composisions?.length > 0) {
+    return <>{composisions}</>;
+  } else {
+    const maybe_syntax_kind = coli.__type;
+    const color =
+      get_color_scheme_for_syntax_kind(maybe_syntax_kind as SyntaxKind) ??
+      fallbackContentColor;
+    const code_string = stringfy(coli, { language: language });
+
+    return (
+      <BasedToken
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onDoubleClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        cornerRadius={0}
+        contentPadding={0}
+        backgroundColor="black"
+        contentColor={color}
+        content={code_string}
+      />
+    );
+  }
+}
+
+function instanceArrayOf(a: any[], t) {
+  return Array.isArray(a) && a.every((x) => x instanceof t);
 }
