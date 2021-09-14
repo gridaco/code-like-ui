7f93e645973ce5abd84724d2f8111b19d5393736
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-14 17:26:43 +0900

    update coli code view with ast formatting

--- packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
+++ packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { BasedToken } from "@code-ui/token";
-import { ColiObject, stringfy } from "coli";
+import { ColiObject, stringfy, stringfy_tokenformatted } from "coli";
+import { format } from "@coli.codes/ast-formatter";
 import { get_color_scheme_for_syntax_kind } from "../color-scheme-mappings";
 import { SyntaxKind } from "@coli.codes/core-syntax-kind";
 import hljs from "highlight.js";
@@ -18,69 +19,52 @@ export function ColiCodeView({
   flatten?: boolean;
   depth?: number;
 }) {
-  const tree = (coli: any) => {
-    const all_fields = Object.keys(coli);
-    const composisions = all_fields
-      .map((k) => {
-        console.log(k);
-        const field = coli[k];
-        if (field instanceof ColiObject) {
-          return (
-            <ColiCodeView key={field.__type} coli={field} depth={depth + 1} />
-          );
-        } else if (instanceArrayOf(field, ColiObject)) {
-          return field.map((c: any) => tree(c));
-        }
-      })
-      .filter((x) => x !== undefined);
-    return composisions;
-  };
-
-  const _tree = tree(coli);
-  const composisions = flatten ? _tree.flat() : _tree;
-
-  const Token = () => {
-    if (composisions?.length > 0) {
-      return <>{composisions}</>;
-    } else {
-      const maybe_syntax_kind = (coli as ColiObject).__type;
-      const color =
-        get_color_scheme_for_syntax_kind(maybe_syntax_kind as SyntaxKind) ??
-        fallbackContentColor;
-      const code_string = stringfy(coli, { language: language });
-
-      return (
-        <BasedToken
-          onClick={function (): void {
-            throw new Error("Function not implemented.");
-          }}
-          onDoubleClick={function (): void {
-            throw new Error("Function not implemented.");
-          }}
-          cornerRadius={0}
-          contentPadding={0}
-          backgroundColor="black"
-          contentColor={color}
-          content={code_string}
-        />
-      );
-    }
-  };
+  const ast = format(coli);
 
   const token = useRef();
-  useEffect(() => {
-    if (token.current) {
-      hljs.highlightElement(token.current);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (token.current) {
+  //     hljs.highlightElement(token.current);
+  //   }
+  // }, []);
 
   return (
     <div ref={token}>
-      <Token />
+      <AstView token={ast} />
     </div>
   );
 }
 
-function instanceArrayOf(a: any[], t: any) {
-  return Array.isArray(a) && a.every((x) => x instanceof t);
+function AstView({ token }: { token: any }) {
+  if (Array.isArray(token)) {
+    return (
+      <>
+        {token.map((t) => {
+          return <AstView token={t} />;
+        })}
+      </>
+    );
+  }
+
+  if (token.kind == "\n") {
+    return <br />;
+  }
+  const code_string = stringfy_tokenformatted(token);
+  return (
+    <>
+      <BasedToken
+        onClick={function (): void {
+          console.log("clicked", token);
+        }}
+        onDoubleClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        cornerRadius={0}
+        contentPadding={0}
+        backgroundColor="#1eff0022"
+        contentColor={"black"}
+        content={code_string ?? ""}
+      />
+    </>
+  );
 }
