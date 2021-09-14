e79c29e47d5756f8d266eb3b44c19fe617135d3e
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-14 18:21:18 +0900

    coli view - wip - dropping

--- packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
+++ packages/code-ui-with-coli/coli-code-view/coli-code-view.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useRef } from "react";
 import { BasedToken } from "@code-ui/token";
 import { ColiObject, stringfy, stringfy_tokenformatted } from "coli";
-import { format } from "@coli.codes/ast-formatter";
+import { format, FormattingToken } from "@coli.codes/ast-formatter";
 import { get_color_scheme_for_syntax_kind } from "../color-scheme-mappings";
 import { SyntaxKind } from "@coli.codes/core-syntax-kind";
 import hljs from "highlight.js";
 import "highlight.js/styles/vs2015.css";
 
+const bg_with_transparancy = "#1eff0022";
 export function ColiCodeView({
   coli,
   fallbackContentColor = "white",
@@ -61,6 +62,38 @@ function AstView({ token }: { token: any }) {
   if (token.kind == "\n") {
     return <br />;
   }
+
+  // ---
+
+  const fields = Object.keys(token)
+    .map((key) => {
+      if (token[key] instanceof ColiObject) {
+        return <AstView token={token[key]} />;
+      }
+      return;
+    })
+    .filter(Boolean);
+
+  if (fields.length > 0) {
+    return (
+      <BasedToken
+        content={<>{fields.map((m) => m)}</>}
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onDoubleClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        backgroundColor={bg_with_transparancy}
+        cornerRadius={0}
+        contentPadding={1}
+        contentColor={""}
+      />
+    );
+  }
+
+  // ---
+
   const code_string = stringfy_tokenformatted(token);
   return (
     <>
@@ -73,7 +106,7 @@ function AstView({ token }: { token: any }) {
         }}
         cornerRadius={0}
         contentPadding={0}
-        backgroundColor="#1eff0022"
+        backgroundColor={bg_with_transparancy}
         contentColor={"black"}
         content={code_string ?? ""}
       />
